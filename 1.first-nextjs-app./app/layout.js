9c81b62771170fc271e88be815c334121dd697bc
이정현 <zenghyun@mz.co.kr>
2025-11-12 19:33:29 +0900

    chore: 공부 시작

--- 1.first-nextjs-app./app/layout.js
+++ 1.first-nextjs-app./app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  console.log("hello!");
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
